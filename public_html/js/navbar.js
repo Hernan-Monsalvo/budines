285b78951969b3eb482f559a751cfa259b681c0a
Hernan <Hernan@192.168.0.4>
2020-09-20 01:30:02 -0300

    Primer commit

---
+++ public_html/js/navbar.js
@@ -0,0 +1,24 @@
+document.write("<div class='menu-btn'><i class='fa fa-bars'></i></div>");
+
+document.write("<nav class='nav-main'>");
+    document.write("<ul class='nav-menu'>");
+
+        document.write("<li><a href='#'>Inicio</a></li>");
+        
+        document.write("<li><a href='#'>Forma</a></li>");
+        
+        document.write("<li><a href='#'>Sabor</a></li>");
+        
+        document.write("<li><a href='#'>Agregados</a></li>");
+        
+        document.write("<li><a href='#'>cobertura</a></li>");
+        
+        document.write("<li><a href='#'>Total</a></li>");
+        
+        document.write("<li><a href='#'>Datos</a></li>");
+
+    document.write("</ul>");
+document.write("</nav>");
+
+
+
